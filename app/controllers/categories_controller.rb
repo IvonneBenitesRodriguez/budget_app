0f80d60c92df89edc299f42bea92430203d7e891
Ivonne <ivonnebenites2015@gmail.com>
2024-01-24 12:01:36 -0500

    Update file

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -38,11 +38,7 @@ class CategoriesController < ApplicationController
   private
 
   def set_category
-    @category = current_user.categories.find_by(id: params[:id])
-
-    return unless @category.nil?
-
-    redirect_to root_path, alert: 'Category not found.'
+    @category = current_user.categories.find(params[:id]) if params[:id].present?
   end
 
   def category_params
