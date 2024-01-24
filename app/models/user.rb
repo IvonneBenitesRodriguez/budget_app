a15945dafaf3cfb6805aebc1ca3abf4b2074c042
Ivonne <ivonnebenites2015@gmail.com>
2024-01-24 12:02:20 -0500

    Update file named user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable,
+         :omniauthable
 
   has_many :categories, dependent: :destroy
   has_many :expenses, foreign_key: 'author_id', dependent: :destroy
