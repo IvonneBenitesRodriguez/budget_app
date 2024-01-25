032a30f9eca2378c45e6d77953b1772e2e2d637e
Ivonne <ivonnebenites2015@gmail.com>
2024-01-25 08:38:53 -0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,4 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  devise_for :users, controllers: {
-    registrations: 'users/registrations',
-    sessions: 'users/sessions',
-    passwords: 'users/passwords',
-    confirmations: 'users/confirmations',
-    unlocks: 'users/unlocks',
-    omniauth_callbacks: 'users/omniauth_callbacks'
-  }
-
-  devise_scope :user do
-    get '/users/sign_out' => 'devise/sessions#destroy'
-  end
-
-  resources :categories do
-    resources :expenses
-  end
-
-=======
   devise_for :users
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
@@ -25,7 +6,6 @@ Rails.application.routes.draw do
   resources :categories do
     resources :expenses
   end
->>>>>>> development
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
